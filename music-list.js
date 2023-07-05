9cb1e93dce9c8d969e46afec3417d45fd6716aba
Jaivardhan0707 <135408413+Jaivardhan0707@users.noreply.github.com>
2023-07-05 20:52:38 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Kya loge tum",
+    artist: "B Praak and Jaani",
+    img: "Song1",
+    src: "Song1"
+  },
+  {
+    name: "Monna Kanipinchavu",
+    artist: "Harris Jayaraj",
+    img: "Song2",
+    src: "Song2"
+  },
+  {
+    name: "Raataan Lambiyan",
+    artist: "Jubin Nautiyal, Tanishk Bagchi, Asees Kaur",
+    img: "Song3",
+    src: "Song3"
+  },
+  {
+    name: "Ram Siya Ram",
+    artist: "Parampara Thakur, Sachet Tandon",
+    img: "Song4",
+    src: "Song4"
+  },
+  {
+    name: "Kesariya",
+    artist: "Arijit Singh",
+    img: "Song5",
+    src: "Song5"
+  },
+  {
+    name: "Bella Ciao",
+    artist: "	Manu Pilas",
+    img: "Song6",
+    src: "Song6"
+  },
+];
